1907d6a68f248f15a481f84d1b4d02eb7436cf12
Loic Perrin <perrin_l@g01-r2p04.local>
2016-02-03 11:36:53 +0100

    L

--- recup.php
+++ recup.php
@@ -3,18 +3,20 @@
 function recup($argv)
 {
   $j = 0;
+  $i = 0;
 	$file = "/etc/" . "rc" . "$argv[1]" . '.d/';
-  if (is_readable($file))
+
+ if (is_readable($file))
   {
   	$tab_file = scandir($file);
   	while ($tab_file[$i])
   	{
-    	if (preg_match("/[K][0-9]{2}[0-9]?(.*)/", $tab_file[$i], $output) == 0)
+    	if (preg_match("/[K][0-9]{2}[0-9]?(.*)/", $tab_file[$i], $output) == 1)
       {
         $array[0][$j] = $output[1];
         $j++;
       }
-  		else if (preg_match("/[S][0-9]{2}[0-9]?(.*)/", $tab_file[$i]) == 0)
+  		else if (preg_match("/[S][0-9]{2}[0-9]?(.*)/", $tab_file[$i], $output) == 1)
       {
         $array[1][$j] = $output[1];
         $j++;
