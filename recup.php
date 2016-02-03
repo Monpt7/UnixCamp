3afeb320830881a2fbb052f05b621a066a9309f0
Loic Perrin <perrin_l@g01-r2p04.local>
2016-02-03 11:19:38 +0100

    K

--- recup.php
+++ recup.php
@@ -9,14 +9,14 @@ function recup($argv)
   	$tab_file = scandir($file);
   	while ($tab_file[$i])
   	{
-    	if (preg_match("/[K][0-9]{2}[0-9]?(.*)/", $tab_file[$i]) == 0)
+    	if (preg_match("/[K][0-9]{2}[0-9]?(.*)/", $tab_file[$i], $output) == 0)
       {
-        $array[0][$j] = $tab_file[$i][1];
+        $array[0][$j] = $output[1];
         $j++;
       }
   		else if (preg_match("/[S][0-9]{2}[0-9]?(.*)/", $tab_file[$i]) == 0)
       {
-        $array[1][$j] = $tab_file[$i][1];
+        $array[1][$j] = $output[1];
         $j++;
       }
   		$i++;
