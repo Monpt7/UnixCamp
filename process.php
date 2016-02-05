d6e575c55792cf308eaef72e6b7062cb098d5c89
root <root@debian>
2016-02-05 17:54:58 +0100

    Correction erreurs et warning de la sortie standard

--- process.php
+++ process.php
@@ -5,25 +5,23 @@ function process($tab, $init, $path)
 	$i = 0;
 	while (isset($tab[0][$i]))
 	{
-		exec("bash ".$path."rc".$init.".d/".$tab[0][$i]." stop");
-		$verif = shell_exec("echo $?");
-		$verif = intval($verif);
+		exec("bash ".$path."rc".$init.".d/".$tab[0][$i]." start 2>>/home/louis/UnixCamp/lol",$tabReturn, $verif);
+		preg_match("/^[K|S][\d]{2}(.*)$/", $tab[0][$i], $nomFinal);
 		if ($verif == 0)
-			echo "Stopping " . $tab[0][$i] . "...\t\t\t[ \033[32mOK\033[0m ]\n";
+			echo "Stopping " . $nomFinal[1] . "...\t\t\t\t[ \033[32mOK\033[0m ]\n";
 		else
-			echo "Stopping " . $tab[0][$i] . "...\t\t\t[ \033[31mFAIL\033[0m ]\n";
+			echo "Stopping " . $nomFinal[1] . "...\t\t\t\t[\033[31mFAIL\033[0m]\n";
 		$i++;
 	}
 	$i = 0;
 	while (isset($tab[1][$i]))
 	{
-		exec("bash ".$path."rc".$init.".d/".$tab[1][$i]." start");
-		$verif = shell_exec("echo $?");
-		$verif = intval($verif);
+		exec("bash ".$path."rc".$init.".d/".$tab[1][$i]." start 2>>/home/louis/UnixCamp/lol",$tabReturn, $verif);
+		preg_match("/^[K|S][\d]{2}(.*)$/", $tab[1][$i], $nomFinal);
 		if ($verif == 0)
-			echo "Starting " . $tab[1][$i] . "...\t\t\t[ \033[32mOK\033[0m ]\n";
+			echo "Starting " . $nomFinal[1] . "...\t\t\t\t[ \033[32mOK\033[0m ]\n";
 		else
-			echo "Starting " . $tab[1][$i] . "...\t\t\t[ \033[31mFAIL\033[0m ]\n";
+			echo "Starting " . $nomFinal[1] . "...\t\t\t\t[\033[31mFAIL\033[0m]\n";
 		$i++;
 	}
 }
@@ -33,13 +31,12 @@ function killOld($tab, $init, $path)
 	$i = 0;
 	while (isset($tab[$i]))
 	{
-		exec("bash ".$path."rc".$init.".d/".$tab[$i]." stop");
-		$verif = shell_exec("echo $?");
-		$verif = intval($verif);
+		exec("bash ".$path."rc".$init.".d/".$tab[$i]." start 2>>/home/louis/UnixCamp/lol",$tabReturn, $verif);
+		preg_match("/^[K|S][\d]{2}(.*)$/", $tab[$i], $nomFinal);
 		if ($verif == 0)
-			echo "Stopping " . $tab[$i] . "...\t\t\t[ \033[32mOK\033[0m ]\n";
+			echo "Stopping " . $nomFinal[1] . "...\t\t\t\t[ \033[32mOK\033[0m ]\n";
 		else
-			echo "Stopping " . $tab[$i] . "...\t\t\t[ \033[31mFAIL\033[0m ]\n";
+			echo "Stopping " . $nomFinal[1] . "...\t\t\t\t[\033[31mFAIL\033[0m]\n";
 		$i++;
 	}
 }
