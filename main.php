01780234a946e73f59a6f72b4481a37d9a15f700
root <root@debian>
2016-02-07 17:43:38 +0100

    Refonte archi

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
-require_once('process.php');
-require_once('recup.php');
-require_once('parseConf.php');
+require_once('include/process.php');
+require_once('include/recup.php');
+require_once('include/parseConf.php');
 
 $tab = recup($defInit, $path);
 $tabOld = recupOld($previous, $path);
