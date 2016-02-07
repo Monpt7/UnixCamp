490896262b6b747ec3b7a6c1a038db949c75173e
root <root@debian>
2016-02-07 13:44:59 +0100

    Logs parfaits

--- process.php
+++ process.php
@@ -2,17 +2,15 @@
 
 function process($tab, $init, $path)
 {
-	$logger = " 2>&1 | logger -i";
+	$logger = " 2>&1";//  >> logger -i 2>&1";
 	$i = 0;
 	while (isset($tab[0][$i]))
 	{
 		$commande = $path."rc".$init.".d/".$tab[0][$i]." stop";
 		exec($commande.$logger, $tabReturn, $verif);
 		preg_match("/^[K|S][\d]{2}(.*)$/", $tab[0][$i], $nomFinal);
-		if ($verif == 0)
-			echo "Stopping ".$nomFinal[1]."...\t\t\t\t[ \033[32mOK\033[0m ]\n";
-		else
-			echo "Stopping ".$nomFinal[1]."...\t\t\t\t[\033[31mFAIL\033[0m]\n";
+		logReturn($tabReturn, $nomFinal[1]);
+		affichageStop($verif, $nomFinal[1]);
 		$i++;
 	}
 	$i = 0;
@@ -21,6 +19,7 @@ function process($tab, $init, $path)
 		$commande = $path."rc".$init.".d/".$tab[1][$i]." start";
 		exec($commande.$logger, $tabReturn, $verif);
 		preg_match("/^[K|S][\d]{2}(.*)$/", $tab[1][$i], $nomFinal);
+		logReturn($tabReturn, $nomFinal[1]);
 		if ($verif == 0)
 			echo "Starting ".$nomFinal[1]."...\t\t\t\t[ \033[32mOK\033[0m ]\n";
 		else
@@ -31,17 +30,29 @@ function process($tab, $init, $path)
 
 function killOld($tab, $init, $path)
 {
-	$logger = " 2>&1 | logger -i";
+	$logger = " 2>&1";// >> logger -i 2>&1";
 	$i = 0;
 	while (isset($tab[$i]))
 	{
 		$commande = $path."rc".$init.".d/".$tab[$i]." stop";
 		exec($commande.$logger, $tabReturn, $verif);
 		preg_match("/^[K|S][\d]{2}(.*)$/", $tab[$i], $nomFinal);
-		if ($verif == 0)
-			echo "Stopping ".$nomFinal[1]."...\t\t\t\t[ \033[32mOK\033[0m ]\n";
-		else
-			echo "Stopping ".$nomFinal[1]."...\t\t\t\t[\033[31mFAIL\033[0m]\n";
+		logReturn($tabReturn, $nomFinal[1]);
+		affichageStop($verif, $nomFinal[1]);
 		$i++;
 	}
+}
+
+function affichageStop($verif, $nom)
+{
+	if ($verif == 0)
+		echo "Stopping ".$nom."...\t\t\t\t[ \033[32mOK\033[0m ]\n";
+	else
+		echo "Stopping ".$nom."...\t\t\t\t[\033[31mFAIL\033[0m]\n";
+}
+
+function logReturn($tabReturn, $nom)
+{
+	$strFinal = implode("\n", $tabReturn);
+	exec("echo \"$nom : $strFinal\" | logger");
 }
